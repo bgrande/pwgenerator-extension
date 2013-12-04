3782e2244b43b5ac7f0d75f821687a43e8a35aca
Benedikt Grande <benedikt.grande@mayflower.de>
2013-12-04 07:54:42 +0100

    browser action testing

--- chrome-extension/background.js
+++ chrome-extension/background.js
@@ -5,14 +5,11 @@
 };*/
 
 chrome.browserAction.onClicked.addListener(function (tab) {
-    chrome.tabs.executeScript(tab.id, {
-        code: "alert('asas');"
+    chrome.tabs.executeScript(tab.id, { file: "./lib/core.js" /*, file: "./app/vault-generator.js",*/ }, function () {
+        /*console.log(tab.id);
+        alert(tab.activeElement);
+        alert(tab.contentWindow);*/
+        (document.activeElement||{}).id = 'pass';
+        initGenerator(chrome.extension.getURL("./images/close.png"), $('pass'), undefined, settings);
     });
-
-    chrome.tabs.executeScript(tab.id, {
-        file: "./lib/core.js",
-        //file: "./app/vault-generator.js",
-        code: "(document.activeElement||{}).id = 'pass'; initGenerator(chrome.extension.getURL(\"./images/close.png\"), $('pass'), undefined, settings);"
-    });
-
 });
