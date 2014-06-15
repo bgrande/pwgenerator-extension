0e0607d6b863838510a83ed61e28fcfb2137e222
Benedikt Grande <dev@bgrande.de>
2014-06-15 10:57:11 +0200

    moved factory back

--- chrome-extension/app/generate.js
+++ chrome-extension/app/generate.js
@@ -8,6 +8,15 @@ if (!overlays) {
     var overlays = [];
 }
 
+var overlayFactory = function (settings, passwordElement, domainname) {
+    var domainService = Object.create(DomainService).init(DEFAULT_SETTINGS.serviceExceptions, domainname),
+        loginField = Object.create(LoginField).init(settings.userFieldList),
+        passwordField = Object.create(PasswordField).init(passwordElement),
+        generator = Object.create(Generator).init(settings, passwordField, domainService);
+
+    return Object.create(Overlay).init(settings, passwordField, loginField, generator);
+};
+
 chrome.storage.sync.get('settings', function (items) {
     try {
         var password = Helper.getElementFromList(DEFAULT_SETTINGS.pwFieldList),
