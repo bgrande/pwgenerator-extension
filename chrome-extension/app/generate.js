4ae4ba2a8d6cf1210c29a7eda16ed272daa0b293
Benedikt Grande <dev@bgrande.de>
2014-06-15 11:07:31 +0200

    removed obsolete code

--- chrome-extension/app/generate.js
+++ chrome-extension/app/generate.js
@@ -12,7 +12,7 @@ var overlayFactory = function (settings, passwordElement, domainname) {
     var domainService = Object.create(DomainService).init(DEFAULT_SETTINGS.serviceExceptions, domainname),
         loginField = Object.create(LoginField).init(settings.userFieldList),
         passwordField = Object.create(PasswordField).init(passwordElement),
-        generator = Object.create(Generator).init(settings, passwordField, domainService);
+        generator = Object.create(Generator).init(settings, domainService);
 
     return Object.create(Overlay).init(settings, passwordField, loginField, generator);
 };
