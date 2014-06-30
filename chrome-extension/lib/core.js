cd19d97bde78edf8e1c1d7385f62e6dc955c0ce7
Benedikt Grande <dev@bgrande.de>
2014-06-30 13:00:11 +0200

    added new password rule

--- chrome-extension/lib/core.js
+++ chrome-extension/lib/core.js
@@ -74,6 +74,9 @@ var DEFAULT_SETTINGS = {
             },
             'check24.de': {
                 length: 15
+            },
+            'kabeldeutschland.de': {
+                space: 0
             }
         },
 
