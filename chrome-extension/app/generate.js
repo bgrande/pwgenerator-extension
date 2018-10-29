a847c00351374dbd02d5028e31a3ce0f876edebc
bgrande <benedikt.grande@trivago.com>
2018-10-29 19:45:32 +0100

    simplify password list generation in content script; remove duplicated factory; fix password id deduplication; send password fields and active passwordfield to background script;

--- chrome-extension/app/generate.js
+++ chrome-extension/app/generate.js
@@ -3,19 +3,11 @@
  * -----------------------------------------------------
  */
 'use strict';
+
 // make overlays globally available but keep old on possible file reload
 if (!generatorOverlays) {
     var generatorOverlays = [];
 }
-
-var overlayFactory = function (settings, passwordElement, domainname) {
-    var domainService = Object.create(DomainService).init(settings.serviceExceptions, domainname),
-        loginField = Object.create(LoginField).init(settings.userFieldList),
-        passwordField = Object.create(PasswordField).init(passwordElement),
-        generator = Object.create(Generator).init(settings, domainService);
-
-    return Object.create(Overlay).init(settings, passwordField, loginField, generator);
-};
 
 chrome.storage.sync.get('settings', function (items) {
     try {
@@ -24,15 +16,6 @@ chrome.storage.sync.get('settings', function (items) {
             settings = (undefined !== items.settings) ? Helper.mergeObject(DEFAULT_SETTINGS, JSON.parse(items.settings)) : DEFAULT_SETTINGS,
             pwLength = passwords.length, i;
 
-        if (settings.useBrowserAction) {
-            // @todo if any passwords, use browser popup with password generation input fields
-            // it has to be
-            // -> 1. is there a way to use the logic within the popup as well? -> just inject into popup
-            // -> 2. make popup more beautiful
-            // -> 3. increase UX
-            // chrome.browserAction.openPopup();
-        }
-
         // set chrome specific url getting for close and extend icons
         settings.imgUrl = chrome.extension.getURL('images/close.png');
         settings.arrowUp = chrome.extension.getURL('images/arrow-up.png');
@@ -40,31 +23,57 @@ chrome.storage.sync.get('settings', function (items) {
 
         if (pwLength > 0) {
             // there are pages using the same id twice...
-            Helper.fixDuplicateIds(passwords);
-
+            passwords = Helper.fixDuplicateIds(passwords);
             // deactivate autosend if there are multiple password fields
             settings.autosend = (settings.autosend === true && pwLength === 1);
+        }
 
-            for (i = 0; i < pwLength; i++) {
-                if (!generatorOverlays[i] && !Helper.isOverlay(passwords[i]) && !Helper.hasOverlay(passwords[i])) {
-                    generatorOverlays[i] = overlayFactory(settings, passwords[i]);
-                }
-            }
-        } else if (pwLength === 0 && password && password.tagName === 'INPUT') {
+        if (pwLength === 0 && password && password.tagName === 'INPUT') {
+            passwords.push(password);
             // field does not have a password type - better use no autosend to prevent misbehaviour
             settings.autosend = false;
+        }
 
-            if (!generatorOverlays[0] && !Helper.isOverlay(password) && !Helper.hasOverlay(password)) {
-                generatorOverlays[0] = overlayFactory(settings, password);
+        for (i = 0; i < pwLength; i++) {
+            if (!generatorOverlays[i] && !Helper.isOverlay(passwords[i]) && !Helper.hasOverlay(passwords[i])) {
+                // if we're not on popup: create object; else: just count the fields
+                if (settings.useBrowserPopup == false) {
+                    generatorOverlays[i] = overlayFactory(settings, passwords[i]);
+                } else {
+                    let fieldId = '';
+                    if (passwords[i].hasAttribute('id')) {
+                        fieldId = passwords[i].getAttribute('id');
+                    }
+                    on(passwords[i], 'click', function (e) {
+                       chrome.runtime.sendMessage({event: 'activePasswordField', fieldId: fieldId});
+                    });
+
+                    generatorOverlays[i] = passwords[i];
+                }
             }
         }
 
         // show count of existing overlays
         if (0 < generatorOverlays.length) {
             chrome.runtime.sendMessage({event: 'countChange', overlayCount: generatorOverlays.length});
+            chrome.runtime.sendMessage({event: 'passwordFields', fields: generatorOverlays});
         }
     } catch (exception) {
         console.error(exception);
     }
-
 });
+
+/*
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    switch(request) {
+        case "setPassword":
+            // retrieve document HTML and send to popup.js
+            let fieldId = request.data.fieldId,
+                password = request.data.password;
+
+            break;
+        default:
+            sendResponse(null);
+    }
+});
+*/
