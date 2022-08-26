487afe60f6d67a93eb82f9a615423288abde0b21
Benedikt Grande <benedikt.grande@trivago.com>
2022-08-26 11:14:13 +0200

    feat(popup): async settings
    get settings async and improve the popup logic

--- chrome-extension/app/popup.js
+++ chrome-extension/app/popup.js
@@ -179,9 +179,15 @@ Popup.init = function (settings, passwordField, loginField, generator) {
     return this;
 };
 
-var popup;
+async function getSettings() {
+    return new Promise((resolve, reject) => {
+        chrome.storage.sync.get('settings', function (items) {
+            resolve((undefined !== items.settings) ? Helper.mergeObject(DEFAULT_SETTINGS, JSON.parse(items.settings)) : DEFAULT_SETTINGS);
+        });
+    });
+}
 
-(function () {
+(async function () {
     let closeWindow = function () {
         window.close();
     };
@@ -192,80 +198,76 @@ var popup;
         closeWindow();
     });
 
-    chrome.storage.sync.get('settings', function (items) {
-        let settings = (undefined !== items.settings) ? Helper.mergeObject(DEFAULT_SETTINGS, JSON.parse(items.settings)) : DEFAULT_SETTINGS;
+    const settings = await getSettings();
 
-        if (settings.useBrowserPopup == false) {
-            $(BASE_NAME_POPUP + 'on-overlay').style.display = 'block';
-            $(BASE_NAME_POPUP + 'generator-overlay').style.display = 'none';
-            $(BASE_NAME_POPUP + 'browser-popup').classList = 'popup-without-overlay';
+    if (settings.useBrowserPopup === false) {
+        $(BASE_NAME_POPUP + 'on-overlay').style.display = 'block';
+        $(BASE_NAME_POPUP + 'generator-overlay').style.display = 'none';
+        $(BASE_NAME_POPUP + 'browser-popup').classList = 'popup-without-overlay';
 
-            on($(BASE_NAME_POPUP + 'reload-overlay'), 'click', function (e) {
-                chrome.runtime.sendMessage({event: 'reload', tab: chrome.tabs.getSelected});
-                closeWindow();
-            });
+        on($(BASE_NAME_POPUP + 'reload-overlay'), 'click', function (e) {
+            chrome.runtime.sendMessage({event: 'reload', tab: chrome.tabs.getSelected});
+            closeWindow();
+        });
 
-            on($(BASE_NAME_POPUP + 'disable-overlay'), 'click', function (e) {
-                chrome.runtime.sendMessage({event: 'disable', tab: chrome.tabs.getSelected});
-                closeWindow();
-            });
+        on($(BASE_NAME_POPUP + 'disable-overlay'), 'click', function (e) {
+            chrome.runtime.sendMessage({event: 'disable', tab: chrome.tabs.getSelected});
+            closeWindow();
+        });
 
-            let reloadTitle = chrome.i18n.getMessage("reloadTitle"),
-                disableTitle = chrome.i18n.getMessage("disableTitle");
+        $(BASE_NAME_POPUP + 'reload-title').innerText = chrome.i18n.getMessage("reloadTitle");
+        $(BASE_NAME_POPUP + 'disable-title').innerText = chrome.i18n.getMessage("disableTitle");
 
-            $(BASE_NAME_POPUP + 'reload-title').innerText = reloadTitle;
-            $(BASE_NAME_POPUP + 'disable-title').innerText = disableTitle;
-        }
+        let closeButtonTitle = chrome.i18n.getMessage("close"),
+            serviceNameLabel = chrome.i18n.getMessage("serviceNameLabel"),
+            passphraseLabel = chrome.i18n.getMessage("passphraseLabel");
 
-        if (settings.useBrowserPopup == true) {
-//  @todo images via chrome.extension.getUrl('path/to/image')!? -> shouldn't it work via html?
-
-            $(BASE_NAME_POPUP + 'on-overlay').style.display = 'none';
-            $(BASE_NAME_POPUP + 'generator-overlay').style.display = 'block';
-            $(BASE_NAME_POPUP + 'browser-popup').classList = 'popup-with-overlay';
-
-            // @todo we need to get the currently active (if any: else dummy!) password field! via messages and content script which provides the current fields
-            // this might just only work with the current result-pass password field and we have to send a message to update the password in contentscript
-            let defaultPwField = $(BASE_NAME_POPUP + 'generator-result-pass');
-
-            let domainService = Object.create(DomainService).init(settings.serviceExceptions),
-                loginField = Object.create(LoginField).init(settings.userFieldList),
-                passwordField = Object.create(PasswordField).init(defaultPwField),
-                saltGenerator = Object.create(SaltGenerator).init(settings, domainService, loginField),
-                generator = Object.create(Generator).init(settings, domainService, saltGenerator);
-
-            popup = Object.create(Popup).init(settings, passwordField, loginField, generator);
-
-            let closeButtonTitle = chrome.i18n.getMessage("close"),
-                serviceNameLabel = chrome.i18n.getMessage("serviceNameLabel"),
-                passphraseLabel = chrome.i18n.getMessage("passphraseLabel");
-
-            setTimeout(function () {
-                on($(BASE_NAME_POPUP + 'generate-pass'), 'click', function (e) {
-                    popup.generate();
-                });
-            }, 100);
-
-            /*
-            $(BASE_NAME_POPUP + 'close-pass').setAttribute('title', closeButtonTitle);
-            $(BASE_NAME_POPUP + 'close-icon').setAttribute('alt', closeButtonTitle);
-            $(BASE_NAME_POPUP + 'service-label').innerText = serviceNameLabel;
-            $(BASE_NAME_POPUP + 'servicename-pass').setAttribute('placeholder', generator.getDomainname());
-            $(BASE_NAME_POPUP + 'passphrase-label').innerText = passphraseLabel;
-            */
-        }
-    });
+        /*
+        $(BASE_NAME_POPUP + 'close-pass').setAttribute('title', closeButtonTitle);
+        $(BASE_NAME_POPUP + 'close-icon').setAttribute('alt', closeButtonTitle);
+        $(BASE_NAME_POPUP + 'service-label').innerText = serviceNameLabel;
+        $(BASE_NAME_POPUP + 'servicename-pass').setAttribute('placeholder', generator.getDomainname());
+        $(BASE_NAME_POPUP + 'passphrase-label').innerText = passphraseLabel;
+        */
+    }
 })();
 
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+chrome.runtime.onMessage.addListener(async function(request, sender, sendResponse) {
     if (request.event) {
         switch (request.event) {
             case 'activePasswordField':
                 let activePasswordField = request.fieldId ? request.fieldId : '';
 
-                setTimeout(function () {
+                const settings = await getSettings();
+
+                if (settings.useBrowserPopup === true) {
+//  @todo images via chrome.extension.getUrl('path/to/image')!? -> shouldn't it work via html?
+
+                    $(BASE_NAME_POPUP + 'on-overlay').style.display = 'none';
+                    $(BASE_NAME_POPUP + 'generator-overlay').style.display = 'block';
+                    $(BASE_NAME_POPUP + 'browser-popup').classList = 'popup-with-overlay';
+
+                    // @todo we need to get the currently active (if any: else dummy!) password field! via messages and content script which provides the current fields
+                    // this might just only work with the current result-pass password field and we have to send a message to update the password in contentscript
+                    let defaultPwField = $(BASE_NAME_POPUP + 'generator-result-pass');
+
+                    let domainService = Object.create(DomainService).init(settings.serviceExceptions),
+                        loginField = Object.create(LoginField).init(settings.userFieldList),
+                        passwordField = Object.create(PasswordField).init(defaultPwField),
+                        saltGenerator = Object.create(SaltGenerator).init(settings, domainService, loginField),
+                        generator = Object.create(Generator).init(settings, domainService, saltGenerator);
+
+                    const popup = Object.create(Popup).init(settings, passwordField, loginField, generator);
+
+                    setTimeout(function () {
+                        on($(BASE_NAME_POPUP + 'generate-pass'), 'click', function (e) {
+                            popup.generate();
+                        });
+                    }, 100);
+
+                    // @todo initially the field value is still empty although we have the correct value already!
                     popup.setActiveField(activePasswordField).setServicename(request.serviceName);
-                }, 150);
+                }
 
                 break;
 
@@ -274,6 +276,8 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
         }
     }
 });
+
+
 
 
 
